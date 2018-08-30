84460869558aa0f3dcdaf2736cee3d2887be65eb
Placide IRANDORA <34879554+placideirandora@users.noreply.github.com>
2018-08-30 03:57:34 -0700

    Add files via upload

---
+++ YouTube Downloader PI 1.0 Source Code.cs
@@ -0,0 +1,396 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YouTube_Downloader_PI
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+
+        {
+
+        }
+
+        protected StatusBar mainStatusBar = new StatusBar();
+
+        protected StatusBarPanel statusPanel1 = new StatusBarPanel();
+
+        protected StatusBarPanel datepanel = new StatusBarPanel();
+            
+ 
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxQuality.SelectedIndex = 2;
+
+            statusPanel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+
+            statusPanel1.Text = "Status: No video to download. Paste the video url and download the video.";
+
+            statusPanel1.ToolTipText = "Current Activity";
+
+            statusPanel1.AutoSize = StatusBarPanelAutoSize.Spring;
+
+            mainStatusBar.Panels.Add(statusPanel1);
+
+            mainStatusBar.ShowPanels = true;
+
+
+
+            this.Controls.Add(mainStatusBar);
+
+
+
+            datepanel.BorderStyle = StatusBarPanelBorderStyle.Raised;
+
+            datepanel.ToolTipText = "Date: " + System.DateTime.Today.ToString();
+
+            datepanel.ToolTipText = "Current Date";
+
+            datepanel.Text = System.DateTime.Today.ToLongDateString();
+
+            datepanel.AutoSize = StatusBarPanelAutoSize.Contents;
+
+            mainStatusBar.Panels.Add(datepanel);
+
+
+
+            mainStatusBar.ShowPanels = true;
+
+       
+
+            this.Controls.Add(mainStatusBar);
+
+
+
+
+
+
+
+
+
+        }
+
+        private void btnDowload_Click(object sender, EventArgs e)
+
+        {
+
+
+            if (textBoxUrl.Text == "")
+
+            {
+
+
+
+                MessageBox.Show("No YouTube video url found. Paste/Type the YouTube video url please!", "Paste/Type The YouTube Video Url.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+
+
+
+            else if (textBoxUrl.Text.Contains("www.youtube.com/") || textBoxUrl.Text.Contains("WWW.YOUTUBE.COM/") 
+                || textBoxUrl.Text.Contains("youtube.com/") || textBoxUrl.Text.Contains("YOUTUBE.COM/")
+                || textBoxUrl.Text.Contains("youtu.be/") || textBoxUrl.Text.Contains("YOUTU.BE/")
+                || textBoxUrl.Text.Contains("www.youtu.be/") || textBoxUrl.Text.Contains("WWW.YOUTU.BE/"))
+
+            {
+                if (textBoxPath.Text != "")
+
+                {
+
+
+                    
+
+
+                    statusPanel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+
+                    statusPanel1.Text = "Status: Connecting to YouTube...";
+
+                    statusPanel1.ToolTipText = "Current Activity";
+
+                    statusPanel1.AutoSize = StatusBarPanelAutoSize.Spring;
+
+
+
+                    progressBar.Minimum = 0;
+                    progressBar.Maximum = 100;
+
+                    try
+
+                    {
+
+
+                        IEnumerable<VideoInfo> videos = DownloadUrlResolver.GetDownloadUrls(textBoxUrl.Text);
+
+                        VideoInfo video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == Convert.ToInt32(comboBoxQuality.Text));
+
+                        if (video.RequiresDecryption)
+
+                            DownloadUrlResolver.DecryptDownloadUrl(video);
+
+                        VideoDownloader downloader = new VideoDownloader(video, Path.Combine(textBoxPath.Text, video.Title + video.VideoExtension));
+
+                        downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+
+                        Thread thread = new Thread(() => { downloader.Execute(); }) { IsBackground = true };
+
+                        thread.Start();
+
+
+                        statusPanel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+
+                        statusPanel1.Text = "Status: Downloading: " + video.Title + video.VideoExtension ;
+
+                        statusPanel1.ToolTipText = "Current Activity";
+
+                        statusPanel1.AutoSize = StatusBarPanelAutoSize.Spring;
+
+
+
+
+                    }
+
+                    catch (Exception)
+
+                    {
+
+                        DialogResult Response = MessageBox.Show("Connection failed. Check the connection settings and try again!", "Connection Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+
+                        if (Response == DialogResult.OK)
+
+                        {
+
+                            textBoxUrl.Text = "";
+
+                            progressBar.Value = 0;
+
+                            lblPercentage.Text = "0%";
+
+                            textBoxPath.Text = "";
+
+                            comboBoxQuality.SelectedIndex = 2;
+
+                            statusPanel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+
+                            statusPanel1.Text = "Status: No video to download. Paste the video url and download the video.";
+
+                            statusPanel1.ToolTipText = "Current Activity";
+
+                            statusPanel1.AutoSize = StatusBarPanelAutoSize.Spring;
+
+
+                        }
+
+                    }
+
+                }
+
+
+                else
+
+                {
+
+                    MessageBox.Show("No storage location found. Select where the video will be saved please!", "Video Storage Location", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+
+
+
+
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Invalid YouTube video url. Paste/Type the right YouTube video url please!", "Paste/Type The Right YouTube Video Url.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                textBoxUrl.Text = "";
+
+            }
+
+
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, ProgressEventArgs e)
+
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+
+                progressBar.Value = (int)e.ProgressPercentage;
+
+                lblPercentage.Text = $"{string.Format("{0:0.##}", e.ProgressPercentage)}%";
+
+                progressBar.Update();
+
+                if (progressBar.Value == progressBar.Maximum)
+
+                {
+
+                   DialogResult Response = MessageBox.Show("The video has been successfully downloaded from YouTube.", "Download Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+                    if (Response == DialogResult.OK)
+
+                    {
+
+
+
+                        textBoxUrl.Text = "";
+
+                        progressBar.Value = 0;
+
+                        lblPercentage.Text = "0%";
+
+
+            statusPanel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
+
+            statusPanel1.Text = "Status: No video to download. Paste the video url and download the video.";
+
+            statusPanel1.ToolTipText = "Current Activity";
+
+            statusPanel1.AutoSize = StatusBarPanelAutoSize.Spring;
+
+                       
+
+                    }
+
+
+
+                }
+
+            }
+
+          ));
+
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+
+        {
+              using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select Where The Video Will Be Saved." })
+            {
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+
+                    textBoxPath.Text = fbd.SelectedPath;
+           
+                   
+                
+
+            }
+        }
+
+        private void linkLabelHelpUrl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        {
+            MessageBox.Show("Go to www.youtube.com, then open/watch the video that you want to download" +
+                "  and copy the url/link of the video " +
+                "by right-clicking on the video and select copy video url or you can also copy the video url/link right from the address bar." +
+                " Lastly, After having copied the video url/link, paste it here for being able to download the video.","Copy And Paste The YouTube Video Url" ,MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void linkLabelVideoQuality_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        {
+
+
+            MessageBox.Show("The video quality stands for the resolution - showing things clearly and with a lot of detail - of the video. " +
+               "So, Higher the resolution, clearer the video. 240 pixels is the lower resolution." +
+               " 360 pixels is the low resolution." +
+               " 480 pixels is the medium/normal resolution." +
+               " 780 pixels is the high resolution/high-definition/HD." +
+               " 1080 pixels is the higher resolution/full high-definition/full HD." +
+               " So, Select the video quality according to the quality of the YouTube video that you want to download. It is possible " +
+               " to download a low quality video from a high quality video, but it is impossible to download" +
+               " a high quality video from a low quality video. Therefore, You should firstly check the quality" +
+               " of the Youtube video and select here the right video quality accordingly.  ", "Select The Right" +
+               " Video Quality", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void linkLabelBrowseLocation_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        
+        {
+
+
+            MessageBox.Show("Select the storage location/path where the video " +
+                " will be saved while and after being downloaded from YouTube. ", "Select The Video Storage Location" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+        }
+
+       
+
+        private void linkLabelProgressBar_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("This is the progress bar of the video download operation. It shows" +
+               " the percentage of the video download operation.", "Video Download Progress", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblPercentage_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void btnClipBoardPaste_Click_1(object sender, EventArgs e)
+        {
+            textBoxUrl.Text = Clipboard.GetText();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
